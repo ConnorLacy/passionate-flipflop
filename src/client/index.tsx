04315ce9d9738cf18504b98e21c9744f17956a7e
ConnorLacy <lacyconnor1409@gmail.com>
2020-10-03 16:16:31 -0400

    resolved import

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { App } from './app'
+import { App } from '../components/app'
 import { hydrate } from 'react-dom'
 
 hydrate(<App />, document.getElementById('app'))
